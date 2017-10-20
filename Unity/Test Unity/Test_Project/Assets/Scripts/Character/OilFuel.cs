238997bef6c10d874483c9d345c372376e86e228
Lewis <S177026@ucs.ac.uk>
2017-10-20 17:15:09 +0100

    Oil
    Changed health system to oil

--- Unity/Test Unity/Test_Project/Assets/Scripts/Character/OilFuel.cs
+++ Unity/Test Unity/Test_Project/Assets/Scripts/Character/OilFuel.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CharacterHealth : MonoBehaviour {
+public class OilFuel : MonoBehaviour {
 
     public float CurrentHealth { get; set; }
     public float MaxHealth { get; set; }
 
-    public Slider healthbar;
+    public Slider Oil;
 
     
 
@@ -18,7 +18,7 @@ public class CharacterHealth : MonoBehaviour {
         MaxHealth = 100f;
         CurrentHealth = MaxHealth;
 
-        healthbar.value = CalculateHealth();
+        Oil.value = CalculateHealth();
 
     }
 
@@ -39,7 +39,7 @@ public class CharacterHealth : MonoBehaviour {
     void DealDamage(float damageValue)
     {
         CurrentHealth -= damageValue;
-        healthbar.value = CalculateHealth();
+        Oil.value = CalculateHealth();
         if (CurrentHealth <= 0)
             Die();
     }

ff007aa23a2e162f5448ef8cd0dee0338b2e3da0
HeatherBishop <s177801@ucs.ac.uk>
2017-11-22 14:44:59 +0000

    Added lighting
    -2 unity asset store lighting kits (creative commons license).
    -assembled them in the level

--- Unity/Main Unity/Assets/Scripts/Puzzle/Objects.cs
+++ Unity/Main Unity/Assets/Scripts/Puzzle/Objects.cs
@@ -9,11 +9,18 @@ public class Objects : MonoBehaviour {
     public bool exitReached = false;
     public GameObject destroyed;
     public Transform blockade;
+
+	//to turn the lights on when completed puzzle
+	public GameObject light1;
+	public GameObject light2;
  
     public void hitTarget(bool didHit)
     {
         if (didHit)
         {
+			light1.SetActive (true);
+			light2.SetActive (true);
+
             exitNode.GetComponent<Renderer>().material = exitActivated;
             exitReached = true;
             Instantiate(destroyed, blockade.position, blockade.rotation);

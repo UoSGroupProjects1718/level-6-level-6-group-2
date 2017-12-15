136671b2b0d3e8db6efab0ff2463353415b8e1e8
Lewis <s177026@ucs.ac.uk>
2017-12-15 09:49:30 +0000

    PickUp

---
+++ Unity/Main Unity/Assets/Scripts/MainCharacter/MirrorPickup.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorPickup : MonoBehaviour {
+
+
+    public GameObject pickUp;
+    public GameObject temp;
+    public Transform pickUpLocation;
+
+	// Use this for initialization
+	void Start () {
+        pickUp.GetComponent<Rigidbody>().useGravity = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            pickUp.GetComponent<Rigidbody>().useGravity = false;
+            pickUp.GetComponent<Rigidbody>().isKinematic = true;
+            pickUp.transform.position = pickUpLocation.transform.position;
+            pickUp.transform.rotation = pickUpLocation.transform.rotation;
+            pickUp.transform.parent = temp.transform;
+        }
+        if(Input.GetKeyUp(KeyCode.E))
+        {
+            pickUp.GetComponent<Rigidbody>().useGravity = true;
+            pickUp.GetComponent<Rigidbody>().isKinematic = false;
+            pickUp.transform.parent = null;
+            pickUp.transform.position = temp.transform.position;
+        }      
+	}
+}
